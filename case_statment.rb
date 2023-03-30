9a2fd25772da1779bdff8f4b4a2dde67a5232ad4
Zeph180 <kigozijosephsuubi1@gmail.com>
2023-03-30 11:21:50 +0300

    Add leding comment

--- case_statment.rb
+++ case_statment.rb
@@ -1,6 +1,7 @@
 grade = "c"
 
 did_i_pass = case grade 
+  #then key work can be replaced by complex code
   when "A" then "Hell yeah"
   when "B" then "Do not say"
   else "'You shall not pass!' -Gandalf"
